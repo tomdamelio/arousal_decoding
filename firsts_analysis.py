916d80c53d74025a1f0af9b06308c0ed762b30ab
tdamelio <tomas.d-amelio@inria.fr>
2021-02-11 05:05:12 -0300

    delete first plot

--- firsts_analysis.py
+++ firsts_analysis.py
@@ -5,18 +5,19 @@ from EEG_channels_twente import channels
 s1 = mne.io.read_raw_bdf("data/s01.bdf")
 print(s1.info)
 
-# First plots
-s1.plot(duration=5, n_channels=48)
-
 # Select only EDA data
 s1_temp = s1.copy()
 print('Number of channels in s1_temp:')
 print(len(s1_temp.ch_names), end=' → pick only EDA → ')
 s1_temp.pick_channels(['GSR1'])
 print(len(s1_temp.ch_names))
+# Plot the EDA 
+s1_temp.plot()
+# Plot the EDA power spectral density 
 s1_temp.plot_psd()
 # Create dataframe of EDA subject 1
 df_s1_EDA = s1_temp.to_data_frame()
+
 
 
 # Select only EEG data
@@ -25,6 +26,10 @@ print('Number of channels in s1_temp:')
 print(len(s1_temp2.ch_names), end=' → pick only EEG → ')
 s1_temp2.pick_channels(channels)
 print(len(s1_temp2.ch_names))
+# Plot the EDA 
+s1_temp2.plot()
+# Plot the EDA power spectral density 
+s1_temp2.plot_psd()
 # Create dataframe EEG subject 1
 df_s1_EEG = s1_temp2.to_data_frame()
 
