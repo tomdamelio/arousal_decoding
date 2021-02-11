7fcf3695961db34da2905a0787959e13349d5f3f
tdamelio <tomas.d-amelio@inria.fr>
2021-02-11 17:10:40 -0300

    interactive plot without jupyter

--- firsts_analysis.py
+++ firsts_analysis.py
@@ -1,11 +1,14 @@
 import mne
 from EEG_channels_twente import channels 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import pyplot
 import seaborn as sns
+mpl.use('TkAgg')
 
-#Read bdf
+# Read bdf
 s1 = mne.io.read_raw_bdf("data/s01.bdf", preload=True)
+# Print info of the subject 1 signal
 print(s1.info)
 
 # Select only EDA data
@@ -15,9 +18,11 @@ print(len(s1_temp.ch_names), end=' → pick only EDA → ')
 s1_temp.pick_channels(['GSR1'])
 print(len(s1_temp.ch_names))
 # Plot  EDA. Plot only first part of the signal
+#%matplotlib qt
 s1_temp.plot(scalings='auto') 
 
 # Plot the EDA power spectral density 
+
 s1_temp.plot_psd()
 # Create dataframe of EDA subject 1
 df_s1_EDA = s1_temp.to_data_frame()
@@ -25,8 +30,8 @@ df_s1_EDA = s1_temp.to_data_frame()
 df_s1_EDA.plot()
 pyplot.show()
 # Third test. Plot EDA
-sns.lineplot(x="time", y="GSR1",
-             data=df_s1_EDA)
+#sns.lineplot(x="time", y="GSR1",
+#             data=df_s1_EDA)
 
 # Select only EEG data
 s1_temp2 = s1.copy()
