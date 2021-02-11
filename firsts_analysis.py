4891528ba3a3815fb2c0ab2cae7c2c5efdff3540
tdamelio <tomas.d-amelio@inria.fr>
2021-02-11 05:18:12 -0300

    Select EDA and EEG.  plot signal and PSD

--- firsts_analysis.py
+++ firsts_analysis.py
@@ -11,14 +11,12 @@ print('Number of channels in s1_temp:')
 print(len(s1_temp.ch_names), end=' → pick only EDA → ')
 s1_temp.pick_channels(['GSR1'])
 print(len(s1_temp.ch_names))
-# Plot the EDA 
+# Plot  EDA. Doesn't work well.
 s1_temp.plot()
 # Plot the EDA power spectral density 
 s1_temp.plot_psd()
 # Create dataframe of EDA subject 1
 df_s1_EDA = s1_temp.to_data_frame()
-
-
 
 # Select only EEG data
 s1_temp2 = s1.copy()
@@ -26,9 +24,9 @@ print('Number of channels in s1_temp:')
 print(len(s1_temp2.ch_names), end=' → pick only EEG → ')
 s1_temp2.pick_channels(channels)
 print(len(s1_temp2.ch_names))
-# Plot the EDA 
+# Plot EEG 
 s1_temp2.plot()
-# Plot the EDA power spectral density 
+# Plot the EEG power spectral density 
 s1_temp2.plot_psd()
 # Create dataframe EEG subject 1
 df_s1_EEG = s1_temp2.to_data_frame()
