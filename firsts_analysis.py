9d5623001f3f6f57441c262e2ed7600ba2246087
tdamelio <tomas.d-amelio@inria.fr>
2021-02-11 21:47:21 -0300

    fix plots

--- firsts_analysis.py
+++ firsts_analysis.py
@@ -25,8 +25,8 @@ s1_temp.plot_psd()
 # Create dataframe of EDA subject 1
 df_s1_EDA = s1_temp.to_data_frame()
 # Plot EDA: whole data
-ns.lineplot(x="time", y="GSR1",
-             data=df_s1_EDA)
+#sns.lineplot(x="time", y="GSR1",
+#             data=df_s1_EDA)
 
 # Select only EEG data
 s1_temp2 = s1.copy()
@@ -35,12 +35,11 @@ print(len(s1_temp2.ch_names), end=' → pick only EEG → ')
 s1_temp2.pick_channels(channels)
 print(len(s1_temp2.ch_names))
 # Plot EEG 
-#%matplotlib qt
-s1_temp2.plot(scalings='auto')
+s1_temp2.plot()
 # Plot the EEG power spectral density 
 s1_temp2.plot_psd()
 # Create dataframe EEG subject 1
-#df_s1_EEG = s1_temp2.to_data_frame()
+df_s1_EEG = s1_temp2.to_data_frame()
 
 
 # %%
