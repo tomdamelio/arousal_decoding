6baf060dee198065a9692fea5d87280d0a6deb43
tdamelio <tomas.d-amelio@inria.fr>
2021-02-15 16:32:10 -0300

    Fail attempt FFT and PSD

--- exploratory_analysis_EDA.py
+++ exploratory_analysis_EDA.py
@@ -33,18 +33,52 @@ df_s1_EDA.rename(columns={'GSR1': 'EDA'}, inplace=True)
 
 # Transform EDA (participant 23-32 in Geneva) --> GSR geneva = 10**9 / GSR twente
 if int(number_subject) < 23:
-    df_s1_EDA["EDA"] = (df_s1_EDA["EDA"])/1000
+    df_s1_EDA["EDA"] = (df_s1_EDA["EDA"])/10**9
 else:
-    df_s1_EDA["EDA"] = (10**9/df_s1_EDA["EDA"])*1000
+    df_s1_EDA["EDA"] = (10**9/df_s1_EDA["EDA"])*1000  
     
 # Create column "time_min"
 df_s1_EDA['time_min'] = (df_s1_EDA.time/1000)/60
+# Create column "time_sed"
+df_s1_EDA['time_sec'] = df_s1_EDA.time/1000
+
+# Filter signal
+##  Samples per second
+dt = 512
+## Time
+t = df_s1_EDA.loc[:,'time_sec']
+## Signal to filter
+f = df_s1_EDA.loc[:,'EDA']
+## Lenght signal (in data points)
+n = df_s1_EDA.shape[0]
+## Compute the FFT
+fhat = np.fft.fft(f, n)
+## PSD
+PSD = fhat * np.conj(fhat) / n
+## Frequencies
+freq = 1/(dt*n) * np.arange(n)
+
+L = np.arange(1,np.floor(n/2),dtype='int')
+
+fig, axs = plt.subplots (2,1)
+
+plt.sca(axs[0])
+plt.plot(t,f,color='c', LineWidth=1.5, label='EDA signal')
+plt.xlim (t.iloc[0], t.iloc[-1])
+plt.legend()
+
+plt.sca(axs[1])
+plt.plot(freq[L], PSD[L], color='c', LineWidth=2, label = 'EDA signal')
+plt.xlim(freq[L[0], freq[L[-1]]])
+plt.legend()
     
 # Plot EDA: whole data v.2
-ax = df_s1_EDA.plot.line(title= 'subject'+number_subject, x='time_min', y='EDA')
-ax.set_xlabel("Time(min)")
-ax.set_ylabel("Skin conductance(µS)")
-#ylim=(0,20)
+#ax = df_s1_EDA.plot.line(title= 'subject'+number_subject, x='time_min', y='EDA')
+##ax.set_xlabel("Time(min)")
+#ax.set_ylabel("Skin conductance(µS)")
+## ylim=(0,20) # mantain commented
 
 #PSD EDA
-s1_temp.plot_psd(n_fft=512, fmin=0, fmax=10)
+#s1_temp.plot_psd(n_fft=512, fmin=0, fmax=10)
+
+
