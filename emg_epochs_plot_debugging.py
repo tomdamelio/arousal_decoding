78a718d3996819bfc0705f9139dab02e50a4a254
tdamelio <tomas.d-amelio@inria.fr>
2021-07-16 03:53:36 -0300

    2 plots in emg epochs plotting debugging

--- emg_epochs_plot_debugging.py
+++ emg_epochs_plot_debugging.py
@@ -29,7 +29,7 @@ if measure == 'emg':
 else:
     import DEAP_BIDS_config_eda as cfg
 
-DEBUG = False
+DEBUG = True
 
 derivative_path = cfg.deriv_root
 n_jobs = 15
@@ -64,8 +64,6 @@ for subject in subjects:
       # read epochs
         epochs = mne.read_epochs(epochs_path)
     
-    if DEBUG:
-        epochs = epochs[1600:1800]
     
     if measure == 'emg':
         picks_emg = mne.pick_types(epochs.info, emg=True)
@@ -91,4 +89,32 @@ for subject in subjects:
     else:
         plt.savefig(op.join(derivative_path, 'emg_plot_debugging--16-07', 'sub-' + subject +
                         f'_emg_epochs_plot_debugging.png'))
+        
+    epochs = epochs[1600:1800]
+    
+    if measure == 'emg':
+        picks_emg = mne.pick_types(epochs.info, emg=True)
+    epochs = epochs.filter(20., 30., picks=picks_emg)
+    emg_epochs = epochs.copy().pick_channels(['EXG5','EXG6'])
+    y = emg_epochs.get_data().var(axis=2).mean(axis=1)
+        
+    # Plot the True EDA power and the EDA predicted from EEG data
+    fig, ax = plt.subplots(1, 1, figsize=[10, 4])
+    #times = raw.times[epochs.events[:, 0] - raw.first_samp]
+    times = [i for i in range(len(epochs))]
+    ax.plot(times, y, color='r', label=f'True {measure}')
+    ax.set_xlabel('Time (epochs)')
+    if measure == 'emg':
+        ax.set_ylabel(f'Var(emg)')
+    else:
+        ax.set_ylabel(f'Mean(eda)')
+    ax.set_title(f'Subject {subject} - {measure} Prediction (SPoC) - whitout meegpowreg pipeline')
+    plt.legend()
+    if os.name == 'nt':
+        plt.savefig(op.join(derivative_path, 'emg_plot_debugging--16-07',  'sub-' + subject +
+                            f'_emg_epochs_plot_debugging_200_epochs.png'))
+    else:
+        plt.savefig(op.join(derivative_path, 'emg_plot_debugging--16-07', 'sub-' + subject +
+                        f'_emg_epochs_plot_debugging_200_epochs.png'))
+    
 #%%
